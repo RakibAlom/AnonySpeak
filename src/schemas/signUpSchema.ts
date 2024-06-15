a3f8d33d46842b0e019e6e3e64d4d8100b85f738
MD Rakibul Alom <47636586+RakibAlom@users.noreply.github.com>
2024-06-16 03:03:28 +0600

    sign up schema code updated.

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -10,5 +10,5 @@ export const usernameValidation = z
 export const signUpSchema = z.object({
     username: usernameValidation,
     email: z.string().email({message: 'Invalid email address'}),
-    password: z.string().min(6, {message: "password must be at least 6 characters"})
+    password: z.string().min(6, {message: "Password must be at least 6 characters"})
 })
