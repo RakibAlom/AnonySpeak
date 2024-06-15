250f63e09ec550a09ed549b45beda9743efa2629
MD Rakibul Alom <47636586+RakibAlom@users.noreply.github.com>
2024-06-16 03:04:50 +0600

    username validation update on signup schema

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -2,7 +2,7 @@ import {z} from "zod"
 
 export const usernameValidation = z
     .string()
-    .min(4, "Username must be atleast 4 characters")
+    .min(4, "Username need atleast 4 characters")
     .max(20, "Username must be no more than 20 characters")
     .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters")
 
